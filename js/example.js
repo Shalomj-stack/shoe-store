94926f0a78724f48d33c03c2e9f292a686b40bb5
dudox <trybemark@gmail.com>
2020-08-04 03:26:16 +0100

    samples between vanilla js functionalites and jquery

---
+++ js/example.js
@@ -0,0 +1,38 @@
+    // Vanilla JS
+    // Event listener 
+    // let btn = document.querySelector("#button");
+    // btn.addEventListener('click', function(){
+    //     console.log("Hello World")
+    // })
+
+
+    // function clickme(){
+    //     alert("Hello World");
+    // }
+
+
+    // let fn = (function(){
+    //     return {
+    //         clickme: function(e){
+    //             var title = document.querySelector('.title');
+    //             title.innerHTML = e.dataset.text;
+    //             //console.log(e.dataset.text)
+    //         }
+    //     }
+    // })();
+
+    // jQuery
+
+        // let btn = $("#button");
+        // btn.on('click', function(e){
+        //     $(".title").html(btn.attr("data-text"));
+           
+        // });
+
+        // let fn = (function(){
+        //     return {
+        //         clickme: function(e){
+        //             $(".title").html(e.dataset.text);
+        //         }
+        //     }
+        // })();
